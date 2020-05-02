d4c063467675d6c2b3793702bfb4605bab1a4379
hy <chessg55@gmail.com>
2020-05-02 14:42:43 +0900

    bg_img_

--- vanillaJS/day12/src/bg.js
+++ vanillaJS/day12/src/bg.js
@@ -1,8 +1,31 @@
 //bg.js
+const body = document.querySelector("body"),
+      wrap = body.querySelector("#wrap");
 
+const IMG_NUMBER = 6;
+
+//function handleImgLoad(){
+//	console.log("finished loading");
+//}
+//API활용시 필요
+
+function paintImage(imgNumber){
+	const image = new Image();
+  	image.src = `./images/${imgNumber + 1}.jpg`;
+  	image.classList.add("bgImage");
+  	body.appendChild(image);
+  	//image.addEventListener("loadend",handleImgLoad);
+  	//API활용시 필요
+}
+
+function genRandom(){
+	const number = Math.floor(Math.random() * IMG_NUMBER);
+ 	return number;
+}
 
 function init(){
-
+	const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 
 init();
