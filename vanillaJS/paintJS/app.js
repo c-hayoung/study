a8d1f420c583ccabae2780062ba1875395ab2e35
hy <chessg55@gmail.com>
2020-06-12 17:31:34 +0900

    canvas event_2dcontext

--- vanillaJS/paintJS/app.js
+++ vanillaJS/paintJS/app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 900;
+canvas.height = 500;
+
+ctx.strokeStyle = "#333";
+ctx.lineWidth = 2.5;
 
 
 let painting = false;
@@ -7,24 +14,27 @@ function stopPainting(){
    painting = false;
 }
 
-function onMouseMove(event){
-   const x = event.offsetX;
-   const y = event.offsetY;
-   // console.log(x,y);
-}
-
-function onMouseDown(event){
-   console.log(event);
+function startPainting(){
    painting = true;
 }
 
-function onMouseUp(event){
-   stopPainting();
+function onMouseMove(event){
+   const x = event.offsetX;
+   const y = event.offsetY;
+   if(!painting){
+      // path의 시작점 갱신
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+   }else{
+      // path의 시작점으로부터 이어지는 선 긋기
+      ctx.lineTo(x, y);
+      ctx.stroke();
+   }
 }
 
 if(canvas){
    canvas.addEventListener("mousemove",onMouseMove);
-   canvas.addEventListener("mousedown",onMouseDown);
-   canvas.addEventListener("mouseup",onMouseUp);
+   canvas.addEventListener("mousedown",startPainting);
+   canvas.addEventListener("mouseup",stopPainting);
    canvas.addEventListener("mouseleave", stopPainting);
 }
