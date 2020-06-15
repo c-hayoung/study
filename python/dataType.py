6d82651b9446d675aadb57d4a9bddd9be1dc996b
hy <chessg55@gmail.com>
2020-06-15 16:12:51 +0900

    data types of python

---
+++ python/dataType.py
@@ -0,0 +1,13 @@
+a_string = 'like this' #string
+a_number = 3           #number
+a_float = 3.12         #float
+a_boolean = False      #boolean
+a_none = None          #None
+
+use_snake_case_in_python = True
+
+print(type(a_number))  #int
+print(type(a_float))   #float
+print(type(a_boolean)) #bool
+print(type(a_string))  #str
+print(type(a_none))    #NoneType
