c5083975660bdd80a5eb5b8ad1df5cce97f644b2
hy <chessg55@gmail.com>
2020-06-16 17:43:54 +0900

    Built-in Function

---
+++ python/Theory/Function.py
@@ -0,0 +1,14 @@
+#Built-in Function 예시
+# print()
+# type()
+# len() 
+# int()
+
+age = "18"
+print(age)          #18
+print(type(age))    #string
+n_age = int(age)
+print(n_age)        #18
+print(type(n_age))  #int
+
+# ---
