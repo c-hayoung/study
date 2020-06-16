3076db7ea26ef6e15607295ccb0173e02dc1a4be
hy <chessg55@gmail.com>
2020-06-16 17:19:57 +0900

    add comment

--- python/sequenceType_list.py
+++ python/sequenceType_list.py
@@ -5,7 +5,7 @@ print(days[3])        #Thur(0부터 시작하기 때문)
 print(len(days))      #5
 
 print(days)
-days.append("Sat")
+days.append("Sat")    #마지막에 요소 추가
 print(days)
-days.reverse()
+days.reverse()        #거꾸로
 print(days)
