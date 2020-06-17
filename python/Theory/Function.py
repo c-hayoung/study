626d82b10befa09d833d1514cbd692fa98cdb171
hy <chessg55@gmail.com>
2020-06-17 15:17:10 +0900

    define a function & run function

--- python/Theory/Function.py
+++ python/Theory/Function.py
@@ -12,3 +12,14 @@ print(n_age)        #18
 print(type(n_age))  #int
 
 # ---
+
+def say_hello():
+  print("hello")
+  print("bye")
+print("bye")
+# 들여쓰기로 구분되기 때문에, 주의할 것.
+say_hello()
+say_hello()
+say_hello()
+say_hello()
+# function 실행. ()까지 적어줘야 실행.
