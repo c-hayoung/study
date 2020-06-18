62fbc96556ed5d1c3edf1d5414543c926cc19df3
hy <chessg55@gmail.com>
2020-06-18 15:08:04 +0900

    how to run react app

--- reactJS/movie_app_2020/src/index.js
+++ reactJS/movie_app_2020/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')/* index.js의 #root에 app.js에 실행된 내용을 집어넣는 기능. */
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
