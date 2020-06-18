0e36e28c09d1617589cf1542212af60da916147b
hy <chessg55@gmail.com>
2020-06-18 16:27:43 +0900

    for statement

---
+++ python/Theory/for_in_loop.py
@@ -0,0 +1,14 @@
+
+days = ("Mon", "Tue", "Wed", "Thu", "Fri")
+# 요일을 출력하되, 각각 별도로 출력하라
+for day in days:
+  # print(day)
+  if day is "Wed":
+    break
+  else:
+    print(day)
+# 변수 day는 for문이 실행될 때에만 생겨나는 지역 변수.
+
+for cha in "Nico":
+   print(cha)
+# sequence, string도 리스트의 일종으로 활용가능.
