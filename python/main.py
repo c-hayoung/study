79ee261ea16389f1d1e3491da408c6bbbc4dd5cc
hy <chessg55@gmail.com>
2020-06-22 14:02:13 +0900

    extracting page numbers

--- python/main.py
+++ python/main.py
@@ -5,21 +5,27 @@ indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 # getting the html
 # print(indeed_result.text)
 
-indeed_soup = BeautifulSoup(indeed_result,"html.parser")
+# --- 페이지가 총 몇 페이지인지 알아내기 위해 작성
+indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
 # 추출해낸 html에서 data추출 기능 활성화
 
 pagination = indeed_soup.find("ul",{"class":"pagination-list"})
 # class명이 pagination-list인 ul의 data추출
 
-pages = pagination.find_all("a")
+links = pagination.find_all("a")
 # pagination 안에서 anchor요소 전부 찾아내기
 
-spans = []
+pages = []
 #미리 선언
 
-for page in pages:
-   spans.append(page.find("span"))
+for link in links[:-1]:
+   # pages.append(link.find("span").string)
+   pages.append(int(link.string))
+   # a안의 string이 유일하다면 바로 string method를 사용해도 된다.
 #anchor 안에서 모든 span추출해내기
+# for x in sequence : x는 sequence의 요소를 순차적인 인자로 받아오는 것.
 
-spans = spans[:-1]
-# 마지막 span을 제외한 나머지 span을 리스트화
+# pages = pages[:-1]
+# 마지막 span을 제외한 나머지 span을 pages에 리스트화
+# [x:y] x번째부터 y번째까지.
+print(pages)
