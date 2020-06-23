ead55fa4e8d78152402c60d2595c3a3dea472825
hy <chessg55@gmail.com>
2020-06-23 17:07:36 +0900

    extract location and applying link

--- python/indeed.py
+++ python/indeed.py
@@ -41,26 +41,44 @@ def extract_indeed_pages():
 
    return max_page
 
+
+def extract_info(html):
+   # title
+   title = html.find("h2",{"class":"title"}).find("a")["title"]
+
+   # company
+   company = html.find("span",{"class":"company"})
+   company_anchor = company.find("a")
+   if company_anchor is not None:
+      company = str(company_anchor.string)
+   else:
+      company = str(company.string)
+   company = company.strip()
+   
+   # location
+   location = html.find("div",{"class":"recJobLoc"})["data-rc-loc"]
+
+   # job_id(링크연결)
+   job_id = html["data-jk"]
+
+   return {'title':title, 'company':company,'location':location,'link':f"https://www.indeed.com/viewjob?jk={job_id}"}
+
+
+
 def extract_indeed_start_num(last_page):
    jobs = []
    for page in range(last_page):
-      # pages request
+      print(f"Scrapping page {page}")
+
+      # pages request 기능
       result = requests.get(f"{URL}&start={page*LIMIT}")
 
       # extract title
       soup = BeautifulSoup(result.text,"html.parser")
       results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
       for result in results:
-         title = result.find("h2",{"class":"title"}).find("a")["title"]
-         company = result.find("span",{"class":"company"})
-         company_anchor = company.find("a")
-         if company_anchor is not None:
-            company = str(company_anchor.string)
-         else:
-            company = str(company.string)
-         company = company.strip()
-      print(title,company)
+         job = extract_info(result)
+         jobs.append(job)
       # print(result.status_code)
-      ## page request 기능 적용.
       ## 실행하는지 확인용 console
    return jobs
--- python/main.py
+++ python/main.py
@@ -2,6 +2,6 @@ from indeed import extract_indeed_pages,extract_indeed_start_num
 
 last_indeed_page = extract_indeed_pages()
 
-print(last_indeed_page)
+indeed_jobs = extract_indeed_start_num(last_indeed_page)
 
-extract_indeed_start_num(last_indeed_page)
+print(indeed_jobs)
