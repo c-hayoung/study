a603d5e2b3d3cdbc059f913d9bd7a20dd477f863
hy <chessg55@gmail.com>
2020-06-26 16:38:46 +0900

    reference page

--- python/save.py
+++ python/save.py
@@ -2,6 +2,7 @@ import csv
 
 def save_to_file(jobs):
    file = open("jobs.csv",mode="w",encoding="UTF-8")
+   # http://blog.naver.com/PostView.nhn?blogId=rjs5730&logNo=220980957605&parentCategoryNo=&categoryNo=&viewDate=&isShowPopularPosts=false&from=postView (인코딩 오류참조)
 
    writer = csv.writer(file)
    writer.writerow(["title","company","location","link"])
