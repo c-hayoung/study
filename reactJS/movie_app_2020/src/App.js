8dc5e677bc346f95843d2ed3bc04e3a03a51000c
hy <chessg55@gmail.com>
2020-06-29 16:37:20 +0900

    data fetch Mount

--- reactJS/movie_app_2020/src/App.js
+++ reactJS/movie_app_2020/src/App.js
@@ -4,40 +4,22 @@ import PropTypes, { string } from "prop-types";
 // function App -> class App
 class App extends React.Component{
   state = {
-    count: 0
-  };
+    isLoading: true,
+    movies: []
+  }
 
-  // function part
-  add = () => {
-    // this.setState({count:this.state.count + 1});
-    this.setState(current => ({count:current.count + 1}));
-  };
-  minus = () => {
-    // this.setState({count:this.state.count - 1});
-                      // 외부의 state에 의존하는 건 좋은 코드는 아님. current
-    this.setState(current => ({count:current.count - 1}));
-                      // current : 현재의 state를 받아온다.
-  };
-
+  // data를 fetch from API(Application Programming Interface)
   componentDidMount(){
-    console.log("component rendered");
-  }
-
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-
-  componentWillUnmount(){
-    console.log("Goodbye cruel world..")
+    setTimeout(() =>{
+      this.setState({isLoading:false})
+    },6000);
   }
 
   // class-render
   render(){
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+    const { isLoading } = this.state;
+    // ES6의 class component
+    return <div>{isLoading ? "Loading. . .":"We are ready"}</div>;
   }
 }
 
