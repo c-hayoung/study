dea922dfb259111d7d32eaf94a24f8d42b5e2e65
hy <chessg55@gmail.com>
2020-06-29 15:30:18 +0900

    object oriented programming: class_instance

---
+++ python/Theory/Django/object_oriented_programming.py
@@ -0,0 +1,25 @@
+# class : blueprint
+class Car():
+  wheels = 4
+  doors = 4
+  windows = 4
+  seats = 4
+
+# instance : class를 통해 만들어낸 개별 복제품
+porche = Car()
+# instanciation : 설계도를 가져와 instance를 만드는 것.
+porche.color = "Red"
+
+print(porche) 
+# object 형태로 제시됨.
+print(porche.windows) 
+# 내부 객체를 지정해 출력 가능.
+print(porche.color)
+# 새롭게 지정한 color 출력.
+
+ferrari = Car()
+ferrari.color = "Yellow"
+
+mini = Car()
+mini.color = "White"
+
