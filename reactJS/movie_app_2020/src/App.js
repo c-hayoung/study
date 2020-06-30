21f71a8ed339ce0ea0555e08aaffc3f253434fe1
hy <chessg55@gmail.com>
2020-06-30 18:25:24 +0900

    inspect object

--- reactJS/movie_app_2020/src/App.js
+++ reactJS/movie_app_2020/src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component{
   // data를 fetch from API(Application Programming Interface)
   getMovies = async () => {
     const movies =  await axios.get("https://yts.mx/api/v2/list_movies.json");
+    console.log(movies.data.data.movies)
   }
   // 비동기식 함수 : axios 접근이 끝나길 기다렸다가, 계속하게끔 명령.
 
