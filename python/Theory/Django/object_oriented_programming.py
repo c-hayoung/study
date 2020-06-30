cc80577defd0326170f5388f09a626ba519ccc6a
hy <chessg55@gmail.com>
2020-06-30 16:24:19 +0900

    what is method

--- python/Theory/Django/object_oriented_programming.py
+++ python/Theory/Django/object_oriented_programming.py
@@ -5,6 +5,15 @@ class Car():
   windows = 4
   seats = 4
 
+  # method : class 안의 function (첫번째 argument로 method를 호출하는 instance자신을 가져온다.)
+  def start(self):
+    print(self.color)
+    print("I STARTED")
+
+method_test = Car()
+method_test.color = "Blue"
+method_test.start()
+
 # instance : class를 통해 만들어낸 개별 복제품
 porche = Car()
 # instanciation : 설계도를 가져와 instance를 만드는 것.
