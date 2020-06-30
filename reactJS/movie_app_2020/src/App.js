58dea68f6df36586e650880d7202e8fd906d6ee2
hy <chessg55@gmail.com>
2020-06-30 18:19:19 +0900

    axios, async function

--- reactJS/movie_app_2020/src/App.js
+++ reactJS/movie_app_2020/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes, { string } from "prop-types";
+import axios from "axios";
 
 // function App -> class App
 class App extends React.Component{
@@ -9,10 +10,13 @@ class App extends React.Component{
   }
 
   // data를 fetch from API(Application Programming Interface)
+  getMovies = async () => {
+    const movies =  await axios.get("https://yts.mx/api/v2/list_movies.json");
+  }
+  // 비동기식 함수 : axios 접근이 끝나길 기다렸다가, 계속하게끔 명령.
+
   componentDidMount(){
-    setTimeout(() =>{
-      this.setState({isLoading:false})
-    },6000);
+    this.getMovies()
   }
 
   // class-render
