63193b7a9a63ffb1dcab919022226414c3d0f03e
hy <chessg55@gmail.com>
2020-06-30 16:46:33 +0900

    use_init_method_kwargs

--- python/Theory/Django/object_oriented_programming.py
+++ python/Theory/Django/object_oriented_programming.py
@@ -1,34 +1,49 @@
 # class : blueprint
 class Car():
-  wheels = 4
-  doors = 4
-  windows = 4
-  seats = 4
 
-  # method : class 안의 function (첫번째 argument로 method를 호출하는 instance자신을 가져온다.)
-  def start(self):
-    print(self.color)
-    print("I STARTED")
+  def __init__(self,*args,**kwargs):
+    # self.property 의 default값 선언
+    self.wheels = 4
+    self.doors = 4
+    self.windows = 4
+    self.seats = 4
+    # kwargs를 부여해 값을 수정할 수 있다.
+    self.color = kwargs.get("color","black")
+    self.price = kwargs.get("price","$20")
 
-method_test = Car()
-method_test.color = "Blue"
-method_test.start()
+  def __str__(self):
+    return f"Car with {self.wheels} whells {self.color}"
 
-# instance : class를 통해 만들어낸 개별 복제품
-porche = Car()
-# instanciation : 설계도를 가져와 instance를 만드는 것.
-porche.color = "Red"
-
-print(porche) 
-# object 형태로 제시됨.
-print(porche.windows) 
-# 내부 객체를 지정해 출력 가능.
-print(porche.color)
-# 새롭게 지정한 color 출력.
-
-ferrari = Car()
-ferrari.color = "Yellow"
+porche = Car(color="green",price="$40")
+print(porche.color,porche.price)
 
 mini = Car()
-mini.color = "White"
+print(mini.color,mini.price)
+
+#   # method : class 안의 function (첫번째 argument로 method를 호출하는 instance자신을 가져온다.)
+#   def start(self):
+#     print(self.color)
+#     print("I STARTED")
+
+# method_test = Car()
+# method_test.color = "Blue"
+# method_test.start()
+
+# # instance : class를 통해 만들어낸 개별 복제품
+# porche = Car()
+# # instanciation : 설계도를 가져와 instance를 만드는 것.
+# porche.color = "Red"
+
+# print(porche) 
+# # object 형태로 제시됨.
+# print(porche.windows) 
+# # 내부 객체를 지정해 출력 가능.
+# print(porche.color)
+# # 새롭게 지정한 color 출력.
+
+# ferrari = Car()
+# ferrari.color = "Yellow"
+
+# mini = Car()
+# mini.color = "White"
 
