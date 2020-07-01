710fe9b5232c8e091675ec7102d318eb643d0369
hy <chessg55@gmail.com>
2020-07-01 15:39:05 +0900

    Query Arguments

--- python/SuperScrapper/main.py
+++ python/SuperScrapper/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("BluScrapper")
 
@@ -7,6 +7,11 @@ app = Flask("BluScrapper")
 def home():
   return render_template("potato.html")
   # 경로를 안 지정해줘도 template을 어디서 찾아올지 알고 있음.
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  return render_template("report.html",searchingBy=word)
 
 app.run(debug=True)
 # repl.it환경이기 때문에 host="0.0.0.0"
