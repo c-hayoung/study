f8e7e626ef4ac92998a2ff20989903c7e1f87357
hy <chessg55@gmail.com>
2020-07-07 19:48:12 +0900

    Flask run_get csv file

--- python/SuperScrapper/exporter.py
+++ python/SuperScrapper/exporter.py
@@ -5,7 +5,7 @@ def save_to_file(jobs):
    # http://blog.naver.com/PostView.nhn?blogId=rjs5730&logNo=220980957605&parentCategoryNo=&categoryNo=&viewDate=&isShowPopularPosts=false&from=postView (인코딩 오류참조)
 
    writer = csv.writer(file)
-   writer.writerow(["title","company","location","link"])
+   writer.writerow(["Title","Company","Location","Link"])
 
    for job in jobs:
       writer.writerow(list(job.values()))
